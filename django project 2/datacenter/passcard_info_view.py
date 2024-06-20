0f529ffd3c7351e653f33711d73cd532e6f89a80
planet-pelageya <ryazancev_0101@mail.ru>
2024-06-20 04:17:14 +0300

    Add files via upload

---
+++ django project 2/datacenter/passcard_info_view.py
@@ -0,0 +1,19 @@
+from datacenter.models import Passcard
+from datacenter.models import Visit, is_visits_long, get_duration
+from django.shortcuts import render, get_object_or_404
+from django.utils.timezone import localtime, timedelta
+
+
+def passcard_info_view(request, passcode):
+    person_passcard = get_object_or_404(Passcard, passcode=passcode)
+    visits = Visit.objects.filter(passcard=person_passcard)
+    person_visits = []
+    for visit in visits:
+        person_visits.append({
+            'entered_at': visit.entered_at,
+            'duration': get_duration(visit),
+            'is_strange': is_visits_long(visit, minutes=60)
+        })
+
+    context = {'passcard':Passcard, 'this_passcard_visits': person_visits}
+    return render(request, 'passcard_info.html', context)
---
+++ django project 2/project/settings.py
@@ -0,0 +1,46 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv("DATABASE_ENGINE"),
+        'HOST': os.getenv("DATABASE_HOST"),
+        'PORT': os.getenv("DATABASE_PORT"),
+        'NAME': os.getenv("DATABASE_NAME"),
+        'USER': os.getenv("DATABASE_USER"),
+        'PASSWORD': os.getenv("DATABASE_PASSWORD"),
+    }
+}
+
+
+INSTALLED_APPS = ['datacenter']
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+DEBUG = os.getenv("DEBUG").lower() == 'true'
+
+ROOT_URLCONF = 'project.urls'
+
+ALLOWED_HOSTS = ['*']
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+    },
+]
+
+
+USE_L10N = True
+
+LANGUAGE_CODE = 'ru-ru'
+
+TIME_ZONE = 'Europe/Moscow'
+
+USE_TZ = True
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
